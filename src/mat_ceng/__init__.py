c2f30f7b7930dc01f297d62e7bfea0d173ea5567
mohamadalitellawi <mohamadalitellawi@gmail.com>
2024-06-07 23:33:35 +0300

    calculate minorr delta_ns

--- src/mat_ceng/__init__.py
+++ src/mat_ceng/__init__.py
@@ -2,9 +2,13 @@
 Package For Some Civil Engineering Tools
 '''
 
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 
 
 
 from mat_ceng.mat_ceng import (say_hi)
-from mat_ceng.column import(Material,Load_Case,Section_Dimensions,calculate_column_actual_moment_of_inertia)
+from mat_ceng.column import(
+    Material,Load_Case,Section_Dimensions,Column,
+    calculate_column_actual_moment_of_inertia,
+    calculate_minor_delta_ns,
+    calculate_major_delta_ns)
--- src/mat_ceng/column.py
+++ src/mat_ceng/column.py
@@ -1,15 +1,17 @@
 from dataclasses import dataclass
+import math
 
 @dataclass
 class Material:
-    fc: float # specified compressive strength of concrete, MPa
-    fy: float # specified yield strength for nonprestressed rein-forcement,MPa
+    fc: float = 50 # specified compressive strength of concrete, MPa
+    fy: float = 420 # specified yield strength for nonprestressed reinforcement, MPa
+    wc: float = 2500 # density, unit weight, of normalweight concrete, kg/m³
 
 @dataclass
 class Section_Dimensions:
     # Note that the dimension c33 is the depth of the Section in the 2-2 direction and contributes primarily to I33.
     section_c22:float # cross section width, mm
-    section_c33 :float # cross section length, mm
+    section_c33 :float # cross section depth, mm
     section_cc :float # 20.5.1.3 clear cover of reinforcement, mm
     rft_ratio :float # reinforcements ratio of longitudinal bars
 
@@ -38,7 +40,12 @@ class Load_Case:
             self.Mu_33 = abs(self.Mu_33)
         return self
 
-
+@dataclass
+class Column():
+    lu_22:float # unsupported length of column or wall, mm
+    lu_33:float # unsupported length of column or wall, mm
+    k_22:float #effective length factor for compression members
+    k_33:float #effective length factor for compression members
 
 def calculate_column_actual_moment_of_inertia(material:Material, dimensions:Section_Dimensions, loads:Load_Case,rounding_digits:int = 3) -> float:
     '''
@@ -54,10 +61,101 @@ def calculate_column_actual_moment_of_inertia(material:Material, dimensions:Sect
 
     I22_calculated = (0.8 + 25 * Ast / Ag)*(1 - (loads.Mu_22/(loads.Pu*dimensions.section_c22)) - 0.5 * (loads.Pu/Po)) * Ig_22 # Table 6.6.3.1.1(b)
     I33_calculated = (0.8 + 25 * Ast / Ag)*(1 - (loads.Mu_33/(loads.Pu*dimensions.section_c33)) - 0.5 * (loads.Pu/Po)) * Ig_33 # Table 6.6.3.1.1(b)
-
+    if loads.Pu <= 0:
+        I22_calculated = 0.35 * Ig_22
+        I33_calculated = 0.35 * Ig_33
     I22 = max(min(I22_calculated,0.875 * Ig_22),0.35 * Ig_22)
     I33 = max(min(I33_calculated,0.875 * Ig_33),0.35 * Ig_33)
 
     return {'moment_of_inertia':(round(I22,rounding_digits),round(I33,rounding_digits)), 
             'ratio':(round(I22/Ig_22,rounding_digits),round(I33/Ig_33,rounding_digits))}
 
+
+def calculate_effective_flexural_stiffness(Ec:float,
+                                           Ig:float,
+                                           I:float,
+                                           betta_dns:float = 0.7,
+                                           equation_a = False,
+                                           equation_c = False):
+    '''
+    (EI)eff, N*mm²
+
+    betta_dns = ratio used to account for reduction of stiffness of columns due to sustained axial loads
+    '''
+    if equation_a:
+        return ((0.4*Ec*Ig)/(1+betta_dns)) # (6.6.4.4.4a)
+    elif equation_c:
+        return ((Ec*I)/(1+betta_dns)) # (6.6.4.4.4c)
+
+def calculate_critical_buckling_load(EI_eff: float,
+                                     k:float,
+                                     lu:float):
+    '''
+    Pc, N
+    lu, unsupported length of column or wall, mm
+    k, effective length factor for compression members
+    '''
+    Pc = (((math.pi)**2 * EI_eff)/(k*lu)**2) # (6.6.4.4.2)
+    return Pc
+
+
+def calculate_column_delta_non_sway(Pu:float,
+                                    Pc:float,
+                                    Cm:float = 1.0):
+    '''
+    delta_non_sway = moment magnification factor used to reflect effects of member curvature between ends of a compression member
+
+    Cm: factor relating actual moment diagram to an equivalent uniform moment diagram
+    '''
+    delta_non_sway = ((Cm)/(1-(Pu/(0.75 * Pc)))) # (6.6.4.5.2)
+    delta_non_sway = max(delta_non_sway,1)
+    return delta_non_sway
+
+def calculate_minor_delta_ns(column:Column,
+                             material:Material,
+                             dim:Section_Dimensions,
+                             load:Load_Case,
+                             Cm:float = 1,
+                             rounding_digits:int = 3):
+    I_calculated = calculate_column_actual_moment_of_inertia(
+        material=material,
+        dimensions=dim,
+        loads=load,
+        rounding_digits=rounding_digits
+    )
+    I = I_calculated['moment_of_inertia'][0] # 0 = minor, 1 = Major
+    Ig = I / I_calculated['ratio'][0]
+    Ec = 4700 * math.sqrt(material.fc)
+    betta_dns = min(1, load.Pu_sustained / load.Pu)
+    EI_eff_a = calculate_effective_flexural_stiffness(Ec,Ig,I,betta_dns,equation_a=True)
+    EI_eff_c = calculate_effective_flexural_stiffness(Ec,Ig,I,betta_dns,equation_c=True)
+    Pc_a = calculate_critical_buckling_load(EI_eff_a,column.k_22, column.lu_22)
+    Pc_c = calculate_critical_buckling_load(EI_eff_c,column.k_22, column.lu_22)
+    delta_ns_a = calculate_column_delta_non_sway(load.Pu,Pc_a,Cm)
+    delta_ns_c = calculate_column_delta_non_sway(load.Pu,Pc_c,Cm)
+    return {'Etabs': delta_ns_a, 'Method C':delta_ns_c}
+
+
+def calculate_major_delta_ns(column:Column,
+                             material:Material,
+                             dim:Section_Dimensions,
+                             load:Load_Case,
+                             Cm:float = 1,
+                             rounding_digits:int = 3):
+    I_calculated = calculate_column_actual_moment_of_inertia(
+        material=material,
+        dimensions=dim,
+        loads=load,
+        rounding_digits=rounding_digits
+    )
+    I = I_calculated['moment_of_inertia'][1] # 0 = minor, 1 = Major
+    Ig = I / I_calculated['ratio'][1]
+    Ec = 4700 * math.sqrt(material.fc)
+    betta_dns = min(1, load.Pu_sustained / load.Pu)
+    EI_eff_a = calculate_effective_flexural_stiffness(Ec,Ig,I,betta_dns,equation_a=True)
+    EI_eff_c = calculate_effective_flexural_stiffness(Ec,Ig,I,betta_dns,equation_c=True)
+    Pc_a = calculate_critical_buckling_load(EI_eff_a,column.k_33, column.lu_33)
+    Pc_c = calculate_critical_buckling_load(EI_eff_c,column.k_33, column.lu_33)
+    delta_ns_a = calculate_column_delta_non_sway(load.Pu,Pc_a,Cm)
+    delta_ns_c = calculate_column_delta_non_sway(load.Pu,Pc_c,Cm)
+    return {'Etabs': delta_ns_a, 'Method C':delta_ns_c}
