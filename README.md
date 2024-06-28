b3fe6d747510f0e177ea5cd1449dc583077f6099
mohamadalitellawi <mohamadalitellawi@gmail.com>
2024-06-28 21:35:50 +0300

    update the credits

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Civil_Engineering_Tools
-Civil Engineering Tools
+#### Date 28-Jun-2024
+
+### Introduction
+This is under construction app collecting some useful tools for civil engineering that are needed regularly in the daily tasks for a civil engineer.
+
+There is no intent to be a full set of tools or as a replacement for some useful commercial softwares in the civil engineering eco system (Prokon, STEPS, TEDDS, ...).
+
+Most of the tools will be built by Python Programing language and following the American standards mostly with metric units.
+
+### Credits
+- big thanks for <a href="https://github.com/connorferster">Connor Ferster</a> for his good training, most of the work here is based on his good teaching.
--- streamlit_app/ceng_app/Ceng.py
+++ streamlit_app/ceng_app/Ceng.py
@@ -9,8 +9,16 @@ st.write("# Welcome to Civil Engineering Tools! 👋")
 
 st.markdown(
     """
+    ### Introduction
     This is under construction app collecting some useful tools for civil engineering that are needed regularly in the daily tasks for a civil engineer.\n
     There is no intent to be a full set of tools or as a replacement for some useful commercial softwares in the civil engineering eco system (Prokon, STEPS, TEDDS, ...).\n
     Most of the tools will be built by Python Programing language and following the American standards mostly with metric units.
 """
+)
+
+st.markdown(
+    '''
+### Credits
+- big thanks for <a href="https://github.com/connorferster">Connor Ferster</a> for his good training, most of the work here is based on his good teaching.
+'''
 )
