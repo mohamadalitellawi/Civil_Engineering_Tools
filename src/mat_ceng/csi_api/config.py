355ee11e763c0a267163b51f30de7c33b26c2574
mohamadalitellawi <mohamadalitellawi@gmail.com>
2025-10-14 08:09:52 +0300

    update to automatically search folder to find config.yaml file

--- src/mat_ceng/csi_api/config.py
+++ src/mat_ceng/csi_api/config.py
@@ -4,6 +4,14 @@ import os
 import yaml
 from pathlib import Path
 
+def get_project_root_folder():
+        here = Path(__file__).resolve()
+        project_root = here.parent
+        while project_root.name != "src":
+            project_root = project_root.parent
+        project_root = project_root.parent  # Now at project root
+        return project_root
+
 def load_config(config_path=None):
     """
     Load configuration from YAML file.
@@ -11,7 +19,9 @@ def load_config(config_path=None):
     """
     if config_path is None:
         # Assume config.yaml is in the same dir as pyproject.toml
-        config_path = Path(__file__).parent.parent.parent.parent / "config.yaml"
+        #config_path = Path(__file__).parent.parent.parent.parent / "config.yaml"
+        project_root = get_project_root_folder()
+        config_path = project_root / "config.yaml"
     else:
         config_path = Path(config_path)
 
